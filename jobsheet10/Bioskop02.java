b1e76a130768a199fb062dbc4180a0bdcec054ef
vani <vani.suw@gmail.com>
2023-11-02 07:25:33 +0700

    jawaban no 12

--- jobsheet10/Bioskop02.java
+++ jobsheet10/Bioskop02.java
@@ -34,8 +34,8 @@ public class Bioskop02 {
 
         System.out.println("Penonton pada baris ke-3: ");
 
-        for (String i : penonton[2]) {
-            System.out.println(i);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
         }
     }
 }
