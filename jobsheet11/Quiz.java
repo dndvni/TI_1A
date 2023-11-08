d2fd8222ea3355670c0cd9f2e845cefe60a14d24
vani <vani.suw@gmail.com>
2023-11-09 03:49:41 +0700

    jawaban n0 2

--- jobsheet11/Quiz.java
+++ jobsheet11/Quiz.java
@@ -8,18 +8,29 @@ public class Quiz {
         Scanner input = new Scanner(System.in);
 
         char menu = 'Y';
+       do {
+        int number = random.nextInt(10) + 1;
+        boolean success = false;
         do {
-            int number = random.nextInt(10) + 1;
-            boolean success = false;
-            do {
-                System.out.print("Tebak angka (1-10): ");
-                int answer = input.nextInt();
-                input.nextLine();
-                success = (answer == number);
-            } while(!success);
-            System.out.print("Apakah anda ingin mengulang permainan (Y/y)?");
-            menu = input.nextLine().charAt(0);
-        } while (menu=='Y' || menu=='Y');
+            System.out.print("Tebak Angka (1-10): ");
+            while (!input.hasNextInt()) {
+                input.next();
+                System.out.print("Tebak ANgka (1-10): ");
+            }
+            int answer = input.nextInt();
+            input.nextLine();
+            success = (answer == number);
+            if (!success) {
+                System.out.println("Tebakan salah. Silahkan coba lagi. ");
+            }
+        } while (!success);
+
+        System.out.print("Apakah Anda ingin mengulang permainan (Y/y) atau berhenti (lainnya)? ");
+        menu = input.nextLine().charAt(0);
+       } while (menu == 'Y' || menu == 'y');
+
+       System.out.println("Terima kasih telah bermain!");
+       input.close();
          
     }
 }
