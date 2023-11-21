bfbe3bf6f8410f391e681bd00df3f8e7df3e3efc
vani <vani.suw@gmail.com>
2023-11-21 07:51:48 +0700

    modifikasi no 2

--- jobsheet13/TerimaKasih_02.java
+++ jobsheet13/TerimaKasih_02.java
@@ -5,7 +5,13 @@ public class TerimaKasih_02 {
         System.out.println("Thank you for being the best teacher in the world.\n"+ 
         "You inspired in me a love for learning and made me feel like I couls ask you anything.");
     }
+    
     public static void main(String[] args) {
-        
+        System.out.println("Kalimat tanpa fungsi :");
+        System.out.println("Thank you for being the best teacher in the world.\n"+
+        "You inpired in me a love for learning and made me feel like I could ask you anything.");
+
+        System.out.println("\nKalimat dengan fungsi :");
+        UcapanTerimaKasih();
     }
 }
