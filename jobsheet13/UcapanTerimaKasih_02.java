6085f6f69bc77f2421c992476bfea7f5fd91a63f
vani <vani.suw@gmail.com>
2023-11-26 23:09:13 +0700

    jawaban no 3

--- jobsheet13/UcapanTerimaKasih_02.java
+++ jobsheet13/UcapanTerimaKasih_02.java
@@ -11,7 +11,13 @@ public class UcapanTerimaKasih_02 {
     public static void UcapanTerimaKasih(){
         String nama = PenerimaUcapan();
         System.out.println("Thank you "+nama+" for being the best teacher in teh world.\n"+
-        "YOu inspired in me a love for learning and made me feel like I could ask you anything.");
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+
+        // memanggil metode baru dan memberikan parameter pesan tambahan 
+        UcapanTambahan("I appreciate your dedication and guidance. ");
+    }
+    public static void UcapanTambahan(String tambahan) {
+        System.out.println(tambahan);
     }
 
     public static void main(String[] args) {
