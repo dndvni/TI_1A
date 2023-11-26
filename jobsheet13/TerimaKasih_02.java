89218c4953df889f15acb74634e780be820fa0e9
vani <vani.suw@gmail.com>
2023-11-26 19:58:19 +0700

    perbaikan

--- jobsheet13/TerimaKasih_02.java
+++ jobsheet13/TerimaKasih_02.java
@@ -11,13 +11,9 @@ public class TerimaKasih_02 {
     }
     
     public static void main(String[] args) {
-        System.out.println("Kalimat tanpa fungsi :");
-        System.out.println("Thank you for being the best teacher in the world.\n"+
-        "You inpired in me a love for learning and made me feel like I could ask you anything.");
-
-        System.out.println("\nKalimat dengan fungsi :");
         UcapanTerimaKasih();
         String ucapan = "Terima Kasih Pak... Bu... Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
     }
    
     
