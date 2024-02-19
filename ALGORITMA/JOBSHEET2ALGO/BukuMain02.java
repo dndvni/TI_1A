eed4b029882ca9a101eca600b5e1a1f2c8f0add3
vani <vani.suw@gmail.com>
2024-02-19 22:08:44 +0700

    menurut langkah percobaan

---
+++ ALGORITMA/JOBSHEET2ALGO/BukuMain02.java
@@ -0,0 +1,19 @@
+package JOBSHEET2ALGO;
+
+import java.util.Scanner;
+
+public class BukuMain02 {
+    public static void main(String[] args){
+       Buku bk1 = new Buku();
+       bk1.judul = "Today Ends Tomorrow Comes";
+       bk1.pengarang = "Denanda Pratiwi";
+       bk1.halaman = 198;
+       bk1.stok = 13;
+       bk1.harga = 71000;
+
+       bk1.tampilInformasi();
+       bk1.terjual(5);
+       bk1.gantiHarga(60000);
+       bk1.tampilInformasi();
+    }
+}
