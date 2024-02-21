93e2e1c6d9e23b5f003f4498e9a5d3170add1d1c
vani <vani.suw@gmail.com>
2024-02-21 20:55:27 +0700

    konstruktor berparameter

--- ALGORITMA/JOBSHEET2ALGO/BukuMain02.java
+++ ALGORITMA/JOBSHEET2ALGO/BukuMain02.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class BukuMain02 {
     public static void main(String[] args){
-       Buku02 bk1 = new Buku02();
-       bk1.judul = "Today Ends Tomorrow Comes";
-       bk1.pengarang = "Denanda Pratiwi";
-       bk1.halaman = 198;
-       bk1.stok = 13;
-       bk1.harga = 71000;
+
+        //Konstruktor Berparameter
+       Buku02 bk1 = new Buku02("Today Ends Tomorrow Comes", "Denanda Pratiwi", 198, 13, 71000);
 
        bk1.tampilInformasi();
        bk1.terjual(5);
        bk1.gantiHarga(60000);
        bk1.tampilInformasi();
+
+       // Konstruktor berparameter
+       Buku02 bk2 = new Buku02("Self Reward", "Maheera Ayesha", 160, 29, 59000);
+       bk2.terjual(11);
+       bk2.tampilInformasi();
     }
 }
