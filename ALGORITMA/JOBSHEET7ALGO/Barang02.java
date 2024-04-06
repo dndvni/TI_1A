5a183a3f850ef0f6f26c9d856e10dc9a69287d3a
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2024-04-06 14:27:23 +0700

    1

---
+++ ALGORITMA/JOBSHEET7ALGO/Barang02.java
@@ -0,0 +1,13 @@
+package JOBSHEET7ALGO;
+
+public class Barang02 {
+
+    int kode;
+    String nama, kategori;
+
+    Barang02(int kode, String nama, String kategori) {
+        this.kode = kode;
+        this.nama = nama;
+        this.kategori = kategori;
+    }
+}
