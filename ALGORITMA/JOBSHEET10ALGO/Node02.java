42ef44bbc3e9e913d95149e22178f678a6c094b1
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2024-05-03 22:36:47 +0700

    PRAKTIKUM

---
+++ ALGORITMA/JOBSHEET10ALGO/Node02.java
@@ -0,0 +1,12 @@
+// package JOBSHEET10ALGO;
+
+public class Node02 {
+
+    int data;
+    Node02 next;
+
+    Node02(int nilai, Node02 berikutnya) {
+        data = nilai;
+        next = berikutnya;
+    }
+}
